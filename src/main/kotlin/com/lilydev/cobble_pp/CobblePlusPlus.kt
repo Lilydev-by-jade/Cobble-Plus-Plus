9cbf808b2fc8a5a8c27c41417c25a332b5ffdee1
Jade Nash <jade@lilydev.com>
2023-05-21 21:54:15 -0500

    setup stuff

--- src/main/kotlin/com/lilydev/cobble_pp/CobblePlusPlus.kt
+++ src/main/kotlin/com/lilydev/cobble_pp/CobblePlusPlus.kt
@@ -1,13 +1,13 @@
-package com.lilydev.example_mod
+package com.lilydev.cobble_pp
 
 import net.fabricmc.api.ModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-object ExampleMod : ModInitializer {
+object CobblePlusPlus : ModInitializer {
 
-    const val MOD_ID: String = "example_mod"
-    const val MOD_NAME: String = "Example Mod"
+    const val MOD_ID: String = "cobblelily"
+    const val MOD_NAME: String = "CobbleLily"
 
     @JvmField
     val LOGGER: Logger = LoggerFactory.getLogger(MOD_NAME)
